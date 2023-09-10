93d306c1fd0cbb77ddb344e99833cafcd4c51252
Ryan-Zayne <phoenixastro97@gmail.com>
2023-09-10 02:26:39 +0100

    feat: got the backend integrated with vite

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,16 +1,29 @@
 import colors from '@colors/colors';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import express from 'express';
 
 dotenv.config();
 colors.enable();
 
-const app = express();
+export const app = express();
 
 const port = process.env.PORT ?? 5001;
 
 // Middleware
 app.use(express.json());
+app.use(cors());
 
-// Listening for server
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+app.get('/api/test', (_, res) => res.json({ greeting: process.env.IS_VITE_PLUGIN }));
+
+if (!process.env.IS_VITE_PLUGIN) {
+	const frontendFiles = `${process.cwd()}/dist`;
+	app.use(express.static(frontendFiles));
+
+	app.get('/*', (_, res) => {
+		res.send(`${frontendFiles}/index.html`);
+	});
+
+	// eslint-disable-next-line no-console
+	app.listen(port, () => console.log(`Server listening on port ${port}`));
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,15 +2,19 @@ import react from '@vitejs/plugin-react-swc';
 import { fileURLToPath } from 'node:url';
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
+import express from './express-plugin';
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [
+		express('../backend/src/server.js'),
 		react(),
 		checker({
 			typescript: true,
 		}),
 	],
+
+	envDir: '../',
 
 	resolve: {
 		alias: {
