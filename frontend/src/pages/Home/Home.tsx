6c2a287fe30494ecdfbfc352bfb150dd9f9d75ee
Ryan-Zayne <phoenixastro97@gmail.com>
2023-09-14 18:56:06 +0100

    .

--- frontend/src/pages/Home/Home.tsx
+++ frontend/src/pages/Home/Home.tsx
@@ -1,12 +1,25 @@
 import { CarouselContextProvider } from '@/components/Carousel/carouselStoreContext';
+import { useEffect, useState } from 'react';
 import Categories from './Categories/Categories';
 import Hero from './Hero/Hero';
 import HomeProductSection from './HomeProductSection/HomeProductSection';
 import images from './images';
 
+const getGreeting = async () => {
+	const res = await fetch('/api/test');
+	return res.json();
+};
+
 function HomePage() {
+	const [greeting, setGreeting] = useState('');
+
+	useEffect(() => {
+		getGreeting().then((res) => setGreeting(res.greeting));
+	}, []);
+
 	return (
 		<>
+			<p className="absolute z-[1000] ml-[2rem]">{greeting}</p>
 			<CarouselContextProvider slideImages={images}>
 				<Hero />
 			</CarouselContextProvider>
