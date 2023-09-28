cb363cae2b9792670e23a56716a61dbb2b659221
Ryan-Zayne <phoenixastro97@gmail.com>
2023-09-28 23:53:28 +0100

    .

--- backend/src/global/middleware/serveHtmlRouter.middleware.js
+++ backend/src/global/middleware/serveHtmlRouter.middleware.js
@@ -1,13 +1,10 @@
 import express from 'express';
 import path from 'node:path';
-import { environment } from '../utils/constants.js';
 
 const serveHtmlRouter = express.Router();
 
-serveHtmlRouter.get('/', async (req, res) => {
-	if (environment === 'production') {
-		res.sendFile(path.resolve('../', 'frontend', 'dist', 'index.html'));
-	}
+serveHtmlRouter.get('/*', async (req, res) => {
+	res.sendFile(path.resolve('../', 'frontend', 'dist', 'index.html'));
 });
 
 export default serveHtmlRouter;
