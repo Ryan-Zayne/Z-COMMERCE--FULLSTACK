d0e0408027c55a685090d968e7de96a9e8333099
Ryan-Zayne <phoenixastro97@gmail.com>
2023-10-07 23:18:57 +0100

    chore: added rewrite rules to vercel.json to enusure backend requests go as expected
    feat: updated cors config to go along with vercel deployment

--- backend/src/common/config/corsOptions.js
+++ backend/src/common/config/corsOptions.js
@@ -3,7 +3,11 @@
  */
 
 const corsOptions = {
-	origin: ['http://localhost:5173', 'https://zayne-commerce.onrender.com'],
+	origin: [
+		'http://localhost:5173',
+		'https://zayne-commerce.onrender.com',
+		'https://zayne-commerce.vercel.app',
+	],
 	credentials: true,
 	optionsSuccessStatus: 200,
 };
