aae15fca50c6c1c9c917871692a7710bef22f0d5
Ryan-Zayne <phoenixastro97@gmail.com>
2023-10-09 18:48:00 +0100

    .

--- backend/src/common/lib/schemas/formSchema.js
+++ backend/src/common/lib/schemas/formSchema.js
@@ -3,13 +3,14 @@ import { z } from 'zod';
 export const SignUpSchema = z
 	.object({
 		username: z
-			.string({ required_error: 'Username is required!' })
+			.string()
+			.min(1, 'Username is required!')
 			.min(3, 'Username must be at least 3 characters!')
 			.regex(/^\w+$/, 'The username must contain only letters, numbers and underscore (_)'),
 
 		email: z.string().email('Please enter a valid email!'),
 		password: z.string().min(8, 'Password must be at least 8 characters!'),
-		confirmPassword: z.string({ required_error: 'Password confirmation is required!' }),
+		confirmPassword: z.string().min(1, 'Password confirmation is required!'),
 		acceptTerms: z.boolean().refine((val) => val === true, 'Please check this box!'),
 	})
 	.refine((data) => data.password === data.confirmPassword, {
