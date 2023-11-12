852a8df81e4150dfb5fb01156a6be5f3c970fc07
Ryan-Zayne <phoenixastro97@gmail.com>
2023-11-12 17:13:57 +0100

    .

--- frontend/dist/assets/useElementList-d660c73d.js
+++
@@ -1,1 +0,0 @@
-import{F as o}from"./index.esm-5fa6de3f.js";const t=()=>({For:o});export{t as u};
--- frontend/dist/assets/useToggle-eb300f9d.js
+++
@@ -1,1 +0,0 @@
-import{r as e}from"./index-8a12997c.js";const u=(t=!1)=>{const[s,a]=e.useState(t),o=e.useCallback(()=>a(r=>!r),[]);return[s,o]};export{u};
