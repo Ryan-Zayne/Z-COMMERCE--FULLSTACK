e73bfac477c6a740d33c7dc48cb368016650d62d
Ryan-Zayne <phoenixastro97@gmail.com>
2023-12-25 12:17:24 +0100

    .

--- frontend/src/lib/hooks/useMediaQuery.ts
+++ frontend/src/lib/hooks/useMediaQuery.ts
@@ -1,12 +1,12 @@
 import { desktopQuery, mobileQuery, tabletQuery } from '@/lib/utils/constants.ts';
 import { useMediaQueryActions } from '@/store/zustand/globalStore/globalStore.ts';
-import { useEffect } from 'react';
+import { useLayoutEffect } from 'react';
 
 const useMediaQuery = () => {
 	const { setIsMobile, setIsTablet, setIsDesktop } = useMediaQueryActions();
 
 	// prettier-ignore
-	useEffect(
+	useLayoutEffect(
 		function mediaQueryUpdateEffect() {
 			mobileQuery.addEventListener('change', setIsMobile);
 			tabletQuery.addEventListener('change', setIsTablet);
