a58e3df42372ea33d449e2c184061940402e27da
Ryan Zayne <phoenixastro97@gmail.com>
2024-01-29 15:52:21 +0100

    Added abeg help fetcher for storage in codebase

--- frontend/src/lib/type-helpers/global-type-helpers.ts
+++ frontend/src/lib/type-helpers/global-type-helpers.ts
@@ -18,4 +18,14 @@ export const assertDefined = <T>(value: T) => {
 	return value;
 };
 
+export const assertENV = (variable: string | undefined, options?: { message: string }) => {
+	const { message = "Required Environment variable is missing or undefined" } = options ?? {};
+
+	if (variable === undefined) {
+		throw new Error(message);
+	}
+
+	return variable;
+};
+
 export const assertRef = <T>(value: T) => value as NonNullable<T>;
--- frontend/src/lib/utils/create-fetcher--abeg-help/create-fetcher.ts
+++ frontend/src/lib/utils/create-fetcher--abeg-help/create-fetcher.ts
@@ -1,141 +1,137 @@
-import { isObject } from "../global-type-helpers";
+import { isObject } from "@/lib/type-helpers/typeof";
 import type {
-  AbegErrorResponse,
-  AbegResponseData,
-  AbegSuccessResponse,
-  BaseRequestConfig,
-  CallApiParams,
+	AbegErrorResponse,
+	AbegResponseData,
+	AbegSuccessResponse,
+	BaseRequestConfig,
+	CallApiParams,
 } from "./create-fetcher.types";
 import { getResponseData } from "./create-fetcher.utils";
 
-const createFetcher = <TBaseData, TBaseError>(
-  baseConfig: BaseRequestConfig,
-) => {
-  const {
-    baseURL,
-    timeout,
-    defaultErrorMessage = "Failed to fetch success response from server!",
-    onResponseSuccess,
-    onResponseError,
-    ...restOfBaseConfig
-  } = baseConfig;
+const createFetcher = <TBaseData, TBaseError>(baseConfig: BaseRequestConfig) => {
+	const {
+		baseURL,
+		timeout,
+		defaultErrorMessage = "Failed to fetch success response from server!",
+		onResponseSuccess,
+		onResponseError,
+		...restOfBaseConfig
+	} = baseConfig;
 
-  const abortControllerStore = new Map<`/${string}`, AbortController>();
+	const abortControllerStore = new Map<`/${string}`, AbortController>();
 
-  // Overloads for better TypeScript DX
-  async function callApi<TData = TBaseData, TError = TBaseError>(
-    url: `/${string}`,
-  ): Promise<AbegResponseData<TData, TError>>;
+	// Overloads for better TypeScript DX
+	async function callApi<TData = TBaseData, TError = TBaseError>(
+		url: `/${string}`
+	): Promise<AbegResponseData<TData, TError>>;
 
-  async function callApi<TData = TBaseData, TError = TBaseError>(
-    url: `/${string}`,
-    bodyData: Record<string, unknown> | FormData,
-  ): Promise<AbegResponseData<TData, TError>>;
+	async function callApi<TData = TBaseData, TError = TBaseError>(
+		url: `/${string}`,
+		bodyData: Record<string, unknown> | FormData
+	): Promise<AbegResponseData<TData, TError>>;
 
-  async function callApi<TData = TBaseData, TError = TBaseError>(
-    url: `/${string}`,
-    bodyData: Record<string, unknown> | FormData,
-    signal: AbortSignal,
-  ): Promise<AbegResponseData<TData, TError>>;
+	async function callApi<TData = TBaseData, TError = TBaseError>(
+		url: `/${string}`,
+		bodyData: Record<string, unknown> | FormData,
+		signal: AbortSignal
+	): Promise<AbegResponseData<TData, TError>>;
 
-  // Implementation
-  async function callApi<TData = TBaseData, TError = TBaseError>(
-    ...[url, bodyData, signal]: CallApiParams
-  ) {
-    const previousController = abortControllerStore.get(url);
+	// Implementation
+	async function callApi<TData = TBaseData, TError = TBaseError>(
+		...[url, bodyData, signal]: CallApiParams
+	) {
+		const previousController = abortControllerStore.get(url);
 
-    if (previousController) {
-      previousController.abort();
-    }
+		if (previousController) {
+			previousController.abort();
+		}
 
-    const controller = new AbortController();
-    abortControllerStore.set(url, controller);
+		const controller = new AbortController();
+		abortControllerStore.set(url, controller);
 
-    const timeoutId =
-      typeof timeout === "number"
-        ? window.setTimeout(() => controller.abort(), timeout)
-        : null;
+		const timeoutId =
+			typeof timeout === "number" ? window.setTimeout(() => controller.abort(), timeout) : null;
 
-    try {
-      const response = await fetch(`${baseURL}${url}`, {
-        signal: signal ?? controller.signal,
-        method: bodyData ? "POST" : "GET",
+		try {
+			const response = await fetch(`${baseURL}${url}`, {
+				signal: signal ?? controller.signal,
+				method: bodyData ? "POST" : "GET",
 
-        body: isObject(bodyData) ? JSON.stringify(bodyData) : bodyData,
+				body: isObject(bodyData) ? JSON.stringify(bodyData) : bodyData,
 
-        headers: isObject(bodyData)
-          ? {
-              "content-type": "application/json",
-              accept: "application/json",
-            }
-          : undefined,
+				headers: isObject(bodyData)
+					? {
+							"content-type": "application/json",
+							accept: "application/json",
+						}
+					: undefined,
 
-        ...restOfBaseConfig,
-      });
+				...restOfBaseConfig,
+			});
 
-      // Response has http errors
-      if (!response.ok) {
-        await onResponseError?.(response);
+			// Response has http errors
+			if (!response.ok) {
+				await onResponseError?.(response);
 
-        return {
-          data: null,
-          error: await getResponseData<AbegErrorResponse<TError>>(response),
-        };
-      }
+				return {
+					data: null,
+					error: await getResponseData<AbegErrorResponse<TError>>(response),
+				};
+			}
 
-      // Response was successful
-      await onResponseSuccess?.(response);
+			// Response was successful
+			await onResponseSuccess?.(response);
 
-      return {
-        data: await getResponseData<AbegSuccessResponse<TData>>(response),
-        error: null,
-      };
+			return {
+				data: await getResponseData<AbegSuccessResponse<TData>>(response),
+				error: null,
+			};
 
-      // Exhaustive error handling for fetch failures
-    } catch (error) {
-      if (error instanceof DOMException && error.name === "AbortError") {
-        // eslint-disable-next-line no-console
-        console.info(
-          `%cAbortError: Request to ${url} timed out after ${timeout}ms`,
+			// Exhaustive error handling for fetch failures
+		} catch (error) {
+			if (error instanceof DOMException && error.name === "AbortError") {
+				// eslint-disable-next-line no-console
+				console.info(
+					`%cAbortError: Request to ${url} timed out after ${timeout}ms`,
 
-          "color: red; font-weight: 500; font-size: 14px;",
-        );
+					"color: red; font-weight: 500; font-size: 14px;"
+				);
 
-        return {
-          data: null,
-          error: {
-            status: "Error",
-            message: `Request to ${url} timed out after ${timeout}ms`,
-          },
-        };
-      }
+				return {
+					data: null,
+					error: {
+						status: "Error",
+						message: `Request to ${url} timed out after ${timeout}ms`,
+					},
+				};
+			}
 
-      if (error instanceof SyntaxError || error instanceof TypeError) {
-        return {
-          data: null,
-          error: {
-            status: "Error",
-            message: error.message,
-          },
-        };
-      }
+			if (error instanceof SyntaxError || error instanceof TypeError) {
+				return {
+					data: null,
+					error: {
+						status: "Error",
+						message: error.message,
+					},
+				};
+			}
 
-      return {
-        data: null,
-        error: {
-          status: "Error",
-          message: (error as Error).message ?? defaultErrorMessage,
-        },
-      };
+			return {
+				data: null,
+				error: {
+					status: "Error",
+					message: (error as { message?: string }).message ?? defaultErrorMessage,
+				},
+			};
 
-      // Clean up the timeout and remove the now unneeded AbortController from store
-    } finally {
-      abortControllerStore.delete(url);
-      timeoutId !== null && window.clearTimeout(timeoutId);
-    }
-  }
+			// Clean up the timeout and remove the now unneeded AbortController from store
+		} finally {
+			abortControllerStore.delete(url);
+			timeoutId !== null && window.clearTimeout(timeoutId);
+		}
+	}
 
-  return callApi;
+	return callApi;
 };
 
 export { createFetcher };
