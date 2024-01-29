2756af26a38fc6408bc260887383625646020e17
Ryan Zayne <phoenixastro97@gmail.com>
2024-01-29 15:53:14 +0100

    .

--- frontend/src/lib/utils/create-fetcher--abeg-help/callApi.ts
+++ frontend/src/lib/utils/create-fetcher--abeg-help/callApi.ts
@@ -1,19 +1,17 @@
-import { useSession } from "@/store/useSession";
-import { createFetcher } from "./create-fetcher--abeg-help";
-import { assertENV } from "./global-type-helpers";
+import { assertENV } from "@/lib/type-helpers/global-type-helpers";
+import { createFetcher } from "./create-fetcher";
 
 const callApi = createFetcher({
-  baseURL: assertENV(process.env.NEXT_PUBLIC_BACKEND_URL, {
-    message:
-      "Please add the NEXT_PUBLIC_BACKEND_URL variable to your .env file",
-  }),
-  timeout: 60000, // Set timeout to 60 seconds
+	baseURL: assertENV(process.env.NEXT_PUBLIC_BACKEND_URL, {
+		message: "Please add the NEXT_PUBLIC_BACKEND_URL variable to your .env file",
+	}),
+	timeout: 60000, // Set timeout to 60 seconds
 
-  onResponseError: (response) => {
-    if (response.status === 401) {
-      useSession.getState().clearSession();
-    }
-  },
+	onResponseError: (response) => {
+		if (response.status === 401) {
+			useSession.getState().clearSession();
+		}
+	},
 });
 
 export { callApi };
