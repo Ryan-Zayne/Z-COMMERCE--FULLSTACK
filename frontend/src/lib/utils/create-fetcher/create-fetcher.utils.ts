ec1ce0e2c56ea91a3c62aa17fca97079d44b58f7
Ryan Zayne <phoenixastro97@gmail.com>
2024-04-01 20:15:44 +0100

    Finishing touches to a few things

--- frontend/src/lib/utils/create-fetcher/create-fetcher.utils.ts
+++ frontend/src/lib/utils/create-fetcher/create-fetcher.utils.ts
@@ -1,7 +1,5 @@
-export const getResponseData = async <TResponse>(response: Response) => {
-	const isJson = response.headers.get("content-type")?.includes("application/json");
-
-	return isJson ? (response.json() as TResponse) : ({ message: await response.text() } as TResponse);
+export const getResponseData = <TResponse>(response: Response) => {
+	return response.json() as Promise<TResponse>;
 };
 
 export const isHTTPError = (errorName: unknown): errorName is "HTTPError" => errorName === "HTTPError";
