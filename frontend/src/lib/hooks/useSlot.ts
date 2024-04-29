3ac4b28e23eb2bce40ace97bed0a38997c9670c6
Ryan Zayne <phoenixastro97@gmail.com>
2024-04-29 19:07:12 +0100

    .

---
+++ frontend/src/lib/hooks/useSlot.ts
@@ -0,0 +1,42 @@
+import { Children, isValidElement, useMemo } from "react";
+
+type Noop = () => void;
+type NoopWithSlot = { slot?: string };
+
+const isSlotElement = (child: React.ReactNode, SlotWrapper: React.ElementType) => {
+	if (!isValidElement(child)) {
+		return false;
+	}
+
+	const childSlotName = (child as NoopWithSlot).slot;
+	const wrapperSlotName = (SlotWrapper as NoopWithSlot).slot;
+
+	if (typeof childSlotName === "string" && typeof wrapperSlotName === "string") {
+		return childSlotName === wrapperSlotName;
+	}
+
+	if (child.type === SlotWrapper) {
+		return true;
+	}
+
+	if ((child.type as Noop).name === (SlotWrapper as Noop).name) {
+		return true;
+	}
+
+	return child.type.toString() === SlotWrapper.toString();
+};
+
+const useSlot = <TProps extends Record<string, unknown>>(
+	children: React.ReactNode,
+	SlotWrapper: React.ElementType<TProps>
+) => {
+	const Slot = useMemo(() => {
+		const childrenArray = Children.toArray(children);
+
+		return childrenArray.find((child) => isSlotElement(child, SlotWrapper));
+	}, [children, SlotWrapper]);
+
+	return Slot as React.ReactElement<TProps>;
+};
+
+export { useSlot };
