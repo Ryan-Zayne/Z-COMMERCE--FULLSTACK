bd3aaa7686a7a3c8fc09568e68a0215d5c79e9e9
Ryan Zayne <phoenixastro97@gmail.com>
2024-05-17 02:57:31 +0100

    Refactor usePresence hook to support both animation and transition types

--- frontend/src/lib/hooks/usePresence.ts
+++ frontend/src/lib/hooks/usePresence.ts
@@ -5,61 +5,45 @@ import { useCallbackRef } from "./useCallbackRef";
 import { useDebouncedState } from "./useDebounce";
 import { useToggle } from "./useToggle";
 
-type UsePresenceOptions<TDuration extends number | undefined> = {
-	duration?: TDuration;
-	type?: "transition" | "animation";
-	callbackFn?: () => void;
-};
+type UsePresenceOptions<TDuration extends number | undefined, THasType extends boolean> = Prettify<
+	{
+		duration?: TDuration;
+		callbackFn?: () => void;
+	} & (THasType extends true ? { type?: "animation" | "transition" } : unknown)
+>;
 
-type UsePresenceResult<TElement, TDuration> = {
-	_: Prettify<
-		{
-			isPresent: boolean;
-			isVisible: boolean;
-			toggleVisibility: ReturnType<typeof useToggle>[1];
-		} & (TDuration extends undefined ? { elementRef: React.RefObject<TElement> } : unknown)
-	>;
-}["_"];
+type UsePresenceResult<TElement, TDuration> = Prettify<
+	{
+		isPresent: boolean;
+		isVisible: boolean;
+		toggleVisibility: ReturnType<typeof useToggle>[1];
+	} & (TDuration extends undefined ? { elementRef: React.RefObject<TElement> } : unknown)
+>;
+
+type UseSpecificPresence = <
+	TElement extends HTMLElement,
+	TDuration extends number | undefined = undefined,
+>(
+	defaultValue?: boolean,
+	options?: UsePresenceOptions<TDuration, false>
+) => UsePresenceResult<TElement, TDuration>;
 
 type UsePresence = <TElement extends HTMLElement, TDuration extends number | undefined = undefined>(
 	defaultValue?: boolean,
-	options?: UsePresenceOptions<TDuration>
+	options?: UsePresenceOptions<TDuration, true>
 ) => UsePresenceResult<TElement, TDuration>;
 
-/**
- *  usePresence hook provides a way to animate an element, before removing it from the DOM.
- *
- * @param options - The options for the usePresence hook.
- * @param options.defaultValue - The default value for the presence state. Defaults to `true`.
- * @param options.duration - The duration in milliseconds for the debounce effect. Defaults to `150`.
- * @param options.callbackFn - An callback function to be called when the animation is over (when the presence state changes to `false`).
- *
- * @returns A tuple containing the boolean that should be used to show and hide the element state and a function to toggle the presence state.
- */
-const usePresence: UsePresence = (defaultValue = true, options = {}) => {
-	const { duration, type = "animation", callbackFn } = options;
-
-	const stableCallback = useCallbackRef(callbackFn);
-
-	const elementRef = useRef<HTMLElement>(null);
-
+const useAnimationPresence: UseSpecificPresence = (defaultValue = true, options = {}) => {
+	const { callbackFn, duration } = options;
 	const [isShown, setIsShown] = useState(defaultValue);
-
-	const [isMounted, setDebouncedIsMounted, $setRegularIsMounted] = useDebouncedState(
+	const [isMounted, setDebouncedIsMounted, setRegularIsMounted] = useDebouncedState(
 		defaultValue,
 		duration
 	);
+	const elementRef = useRef<HTMLElement>(null);
+	const stableCallback = useCallbackRef(callbackFn);
 
-	const setRegularIsMounted = (value: boolean) => {
-		if (type === "animation") {
-			$setRegularIsMounted(value);
-			return;
-		}
-
-		setDebouncedIsMounted({ $delay: 0 }, value);
-	};
-
-	const handleMountedWithoutRef = (value: boolean) => {
+	const handleIsMountedWithoutRef = (value: boolean) => {
 		if (value) {
 			setRegularIsMounted(true);
 			return;
@@ -68,29 +52,29 @@ const usePresence: UsePresence = (defaultValue = true, options = {}) => {
 		setDebouncedIsMounted(false);
 	};
 
-	const handleMountedWithRef = (value: boolean) => {
+	const handleIsMountedWithRef = (value: boolean) => {
 		if (value) {
 			setRegularIsMounted(true);
 			return;
 		}
 
-		on(type === "animation" ? "animationend" : "transitionend", elementRef.current, () => {
+		on("animationend", elementRef.current, () => {
 			setDebouncedIsMounted.cancel();
-			$setRegularIsMounted(false);
+			setRegularIsMounted(false);
 		});
 	};
 
 	const toggleVisibility = useCallbackRef(<TValue>(newValue?: TValue) => {
-		const handleMounted = !duration ? handleMountedWithRef : handleMountedWithoutRef;
+		const handleSetIsMounted = !duration ? handleIsMountedWithRef : handleIsMountedWithoutRef;
 
 		if (typeof newValue === "boolean") {
 			setIsShown(newValue);
-			handleMounted(newValue);
+			handleSetIsMounted(newValue);
 			return;
 		}
 
 		setIsShown(!isShown);
-		handleMounted(!isShown);
+		handleSetIsMounted(!isShown);
 	});
 
 	useEffect(() => {
@@ -99,11 +83,88 @@ const usePresence: UsePresence = (defaultValue = true, options = {}) => {
 	}, [isMounted]);
 
 	return {
-		isPresent: type === "animation" ? isMounted : isMounted || isShown,
-		isVisible: type === "animation" ? isShown : isMounted && isShown,
+		isPresent: isMounted,
+		isVisible: isShown,
 		toggleVisibility,
 		...(duration === undefined && { elementRef }),
 	} as never;
+};
+
+const useTransitionPresence: UseSpecificPresence = (defaultValue = true, options = {}) => {
+	const { callbackFn, duration } = options;
+	const [isShown, setIsShown] = useState(defaultValue);
+	const [isMounted, setDebouncedIsMounted, setRegularIsMounted] = useDebouncedState(
+		defaultValue,
+		duration
+	);
+	const elementRef = useRef<HTMLElement>(null);
+	const stableCallback = useCallbackRef(callbackFn);
+
+	const handleIsMountedWithoutRef = (value: boolean) => {
+		if (value) {
+			setDebouncedIsMounted({ $delay: 0 }, value);
+			return;
+		}
+
+		setDebouncedIsMounted(false);
+	};
+
+	const handleIsMountedWithRef = (value: boolean) => {
+		if (value) {
+			setDebouncedIsMounted({ $delay: 0 }, value);
+			return;
+		}
+
+		on("transitionend", elementRef.current, () => {
+			setDebouncedIsMounted.cancel();
+			setRegularIsMounted(false);
+		});
+	};
+
+	const toggleVisibility = useCallbackRef(<TValue>(newValue?: TValue) => {
+		const handleSetIsMounted = !duration ? handleIsMountedWithRef : handleIsMountedWithoutRef;
+
+		if (typeof newValue === "boolean") {
+			setIsShown(newValue);
+			handleSetIsMounted(newValue);
+			return;
+		}
+
+		setIsShown(!isShown);
+		handleSetIsMounted(!isShown);
+	});
+
+	useEffect(() => {
+		!isMounted && stableCallback();
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [isMounted]);
+
+	return {
+		isPresent: isMounted || isShown,
+		isVisible: isMounted && isShown,
+		toggleVisibility,
+		...(duration === undefined && { elementRef }),
+	} as never;
+};
+
+/**
+ * usePresence hook provides a way to animate an element, before removing it from the DOM.
+ *
+ * @param defaultValue - The default value for the presence state. Defaults to `true`.
+ * @param options - The options for the usePresence hook.
+ * @param options.type - The type of animation is a transition or an animation. Defaults to "animation".
+ * @param options.duration - The duration in milliseconds for the debounce effect. Defaults to `150`.
+ * @param options.callbackFn - A callback function to be called when the animation is over (when the presence state changes to `false`).
+ *
+ * @returns A tuple containing the boolean that should be used to show and hide the element state and a function to toggle the presence state.
+ */
+
+const usePresence: UsePresence = (defaultValue = true, options = {}) => {
+	const { duration, type = "animation", callbackFn } = options;
+
+	const useSpecificPresence = type === "animation" ? useAnimationPresence : useTransitionPresence;
+
+	return useSpecificPresence(defaultValue, { duration, callbackFn });
 };
 
 export { usePresence };
