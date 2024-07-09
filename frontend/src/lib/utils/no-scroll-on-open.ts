159cce8bcb8a2f0768705350fffaa49b0586096e
Ryan Zayne <phoenixastro97@gmail.com>
2024-07-09 01:14:56 +0100

    render please... 😪

--- frontend/src/lib/utils/no-scroll-on-open.ts
+++ frontend/src/lib/utils/no-scroll-on-open.ts
@@ -4,6 +4,25 @@ type NoScrollOnOpenOptions = {
 	isActive: boolean;
 };
 
+function getViewportScrollbarWidth() {
+	// Create a div that's wider and taller than the viewport
+	const div = document.createElement("div");
+
+	console.dir(div.style);
+	div.style.inset = "0";
+	div.style.position = "absolute";
+
+	// Add it to the body
+	document.body.append(div);
+
+	// Calculate the scrollbar width
+	const scrollbarWidth = div.offsetWidth - div.clientWidth;
+
+	// Remove the div from the body
+	div.remove();
+
+	return scrollbarWidth;
+}
 const noScrollOnOpen = ({ isActive }: NoScrollOnOpenOptions) => {
 	const { isMobileOrTablet } = checkDeviceIsMobileOrTablet();
 	const isDesktop = !isMobileOrTablet;
@@ -15,6 +34,8 @@ const noScrollOnOpen = ({ isActive }: NoScrollOnOpenOptions) => {
 	}
 
 	document.body.style.setProperty("--overflow-y", "hidden");
+
+	console.log(getViewportScrollbarWidth());
 
 	const scrollbarWidth = (window.innerWidth - document.documentElement.clientWidth) / 10;
 
