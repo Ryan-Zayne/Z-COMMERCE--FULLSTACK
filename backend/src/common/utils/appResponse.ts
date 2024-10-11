1a20774fd172167aa5571924b1a9e7a1d85986e6
ryan-zayne <phoenixastro97@gmail.com>
2024-10-11 15:05:03 +0100

    feat: Optimize AppResponse utility function
    - Update `AppResponse` function to sort object properties in a consistent order
    - Improve readability and maintainability of the `AppResponse` function

--- backend/src/common/utils/appResponse.ts
+++ backend/src/common/utils/appResponse.ts
@@ -1,10 +1,11 @@
 import type { Response } from "express";
 
 function AppResponse(res: Response, statusCode: number, message?: string, data?: unknown) {
+	/* eslint-disable perfectionist/sort-objects  */
 	res.status(statusCode).json({
-		...(Boolean(data) && { data }),
-		message: message ?? "Success",
 		status: "success",
+		message: message ?? "Success",
+		...(Boolean(data) && { data }),
 	});
 }
 
