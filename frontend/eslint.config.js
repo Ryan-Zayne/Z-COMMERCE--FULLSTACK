a763e43b3fba7d264b95b09620dc9fa6f87af694
ryan-zayne <phoenixastro97@gmail.com>
2024-10-14 06:45:03 +0100

    .

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,12 +1,15 @@
-import zayne from "@zayne-labs/eslint-config";
+import { zayne } from "@zayne-labs/eslint-config";
 
 export default zayne({
-	ignores: ["dist/**", "eslint.config.js"],
+	ignores: ["dist/**"],
 	react: true,
 	tailwindcss: {
 		overrides: {
 			"tailwindcss/no-unnecessary-arbitrary-value": "off", // Turned off cuz using a custom root font-size (10px)
 		},
+	},
+	tanstack: {
+		react: true,
 	},
 	typescript: {
 		tsconfigPath: "tsconfig.eslint.json",
