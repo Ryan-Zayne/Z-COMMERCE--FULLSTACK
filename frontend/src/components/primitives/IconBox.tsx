4c4bb2c3b2919bb4bc74e1aec41567f59489a276
Ryan-Zayne <phoenixastro97@gmail.com>
2025-02-16 13:52:46 +0100

    add monicon support for local icons

--- frontend/src/components/primitives/IconBox.tsx
+++ frontend/src/components/primitives/IconBox.tsx
@@ -1,2 +1,13 @@
+import { Icon, type IconProps } from "@iconify/react";
+import { Monicon, type MoniconProps } from "@monicon/react";
+import { isString } from "@zayne-labs/toolkit/type-helpers";
 
-export { Icon as IconBox } from "@iconify/react";
+export function IconBox(props: IconProps & Omit<MoniconProps, "name"> & { type?: "local" | "online" }) {
+	const { icon, type = "online", ...restOfProps } = props;
+
+	if (type === "local" && isString(icon)) {
+		return <Monicon name={icon} {...restOfProps} />;
+	}
+
+	return <Icon icon={icon} {...restOfProps} />;
+}
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,10 @@
+import { monicon } from "@monicon/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 import viteTsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-	plugins: [react(), viteTsconfigPaths()],
+	plugins: [react(), viteTsconfigPaths(), monicon({})],
 
 	server: {
 		proxy: {
