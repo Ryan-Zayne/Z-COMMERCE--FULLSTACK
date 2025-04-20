79fffd6598c6c8420978c4c9de74d3a3b1a9a68a
Ryan-Zayne <phoenixastro97@gmail.com>
2025-04-20 14:03:09 +0100

    feat(notFoundMiddleware): handle Render ping requests

--- backend/src/middleware/notFoundHandler.ts
+++ backend/src/middleware/notFoundHandler.ts
@@ -1,8 +1,14 @@
-import type { Request } from "express";
-import { AppError } from "../utils";
+import { consola } from "consola";
+import type { Request, Response } from "express";
+import { AppError, AppResponse } from "../utils";
 import { catchAsync } from "./catchAsyncErrors";
 
-const notFoundHandler = catchAsync((req: Request) => {
+const notFoundHandler = catchAsync((req: Request, res: Response) => {
+	if (req.originalUrl === "/") {
+		consola.info("Ping by Render!");
+		return AppResponse(res, 200, "Ping by Render!");
+	}
+
 	const message = `No '${req.method.toUpperCase()}' request handler defined for '${req.originalUrl}'. Check the API documentation for more details.`;
 
 	throw new AppError(404, message);
